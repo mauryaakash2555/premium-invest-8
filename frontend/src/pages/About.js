34edf98a1655f1dc33f430d35b5799c588d32a24
mauryaakash2555 <mauryaakash2555@gmail.com>
2025-12-11 12:14:35 +0530

    Change background position to center center

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -35,7 +35,7 @@ const About = () => {
             backgroundImage:
               'url(https://images.unsplash.com/photo-1666289158111-7576ce2ccfae?w=1920&h=1080&fit=crop&auto=format&fm=webp&q=75)',
             opacity: 0.5,
-            backgroundPosition: 'top center',
+            backgroundPosition: 'center center',
           }}
         />
         <div
