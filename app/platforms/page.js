e9d71cd85eb6562a2ff0900825002b3b485074dc
akash <mauryaakash2555@gmail.com>
2025-12-25 17:51:08 +0530

    fix: Add use client directive for client-side pages

--- app/platforms/page.js
+++ app/platforms/page.js
@@ -3,5 +3,5 @@
 import Platforms from '../../frontend/src/pages/Platforms';
 
 export default function PlatformsPage() {
-    return <Platforms />;
-      }
+      return <Platforms />;
+          }
